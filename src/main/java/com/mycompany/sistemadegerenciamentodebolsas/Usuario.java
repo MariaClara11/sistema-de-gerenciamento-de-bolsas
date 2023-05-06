267cf676b5f7c788c43986a2875edbbb3736f28b
Maria Clara Ribeiro <mcrm74161@gmail.com>
2023-05-06 16:50:11 -0300

    SuperClasse Usuario e subclasses Aluno e Funcionario

--- src/main/java/com/mycompany/sistemadegerenciamentodebolsas/Usuario.java
+++ src/main/java/com/mycompany/sistemadegerenciamentodebolsas/Usuario.java
@@ -11,16 +11,110 @@ package com.mycompany.sistemadegerenciamentodebolsas;
 public class Usuario {
     private String nome;
     private int idade;
+    private String CPF;
+    private String dataNascimento;
     private String login;
     private String senha;
-    
+    private String telefone;
+
     public Usuario(){
-    }
+        this.nome = "";
+        this.idade = 0;
+        this.CPF = "";
+        this.dataNascimento = "";
+        this.login = "";
+        this.senha = "";
+        this.telefone = "";
+    };
     
-    public Usuario(String nome, int idade, String login, String senha){
+    public Usuario(String nome, int idade, String CPF, String dataNascimento, String login, String senha, String telefone, String DataNascimento) {
         this.nome = nome;
         this.idade = idade;
+        this.CPF = CPF;
+        this.dataNascimento = dataNascimento;
         this.login = login;
         this.senha = senha;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    public void editaDadosUsuario(String nome, int idade, String CPF, String dataNascimento, String login, String senha, String telefone, String DataNascimento) {
+        this.nome = nome;
+        this.idade = idade;
+        this.CPF = CPF;
+        this.dataNascimento = dataNascimento;
+        this.login = login;
+        this.senha = senha;
+        this.telefone = telefone;
+    }
+    
+    public void resetaDadosUsuario() {
+        this.nome = "";
+        this.idade = 0;
+        this.CPF = "";
+        this.dataNascimento = "";
+        this.login = "";
+        this.senha = "";
+        this.telefone = "";
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nome=" + nome + ", idade=" + idade + ", CPF=" + CPF + ", dataNascimento=" + dataNascimento + ", login=" + login + ", senha=" + senha + ", telefone=" + telefone + '}';
     }
 }
