e5473fba7911d8623f956dc6544880aab40dbc7f
igortitoneli <igor.titoneli@estudante.ufjf.br>
2023-06-20 12:18:41 -0300

    cadastro bolsa no banco de dados formato JSON

--- src/main/java/Persistence/Arquivo.java
+++ src/main/java/Persistence/Arquivo.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class Arquivo {
 
     public static String le(String filePath) {
+        
         StringBuilder content = new StringBuilder();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
@@ -15,11 +16,33 @@ public class Arquivo {
         } catch (IOException e) {
             //e.printStackTrace();
         }
-
+        
         return content.toString();
     }
-
-    public static void salva(String filePath, String content) {
+    
+    public static void salva(String filePath, String novo) {
+        
+        String conteudo = le(filePath);
+  
+        if(!conteudo.isEmpty()){
+            conteudo = conteudo.substring(0, conteudo.length()-2);
+            conteudo = conteudo.concat(",\n");
+            
+            novo = novo.substring(1);
+            
+            conteudo = conteudo.concat(novo);
+        
+            salvaAux(filePath, conteudo); 
+        }
+        else{
+            salvaAux(filePath, novo); 
+        }
+        
+        
+    }
+    
+    
+    public static void salvaAux(String filePath, String content) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, false))) {
             writer.write(content);
         } catch (IOException e) {
